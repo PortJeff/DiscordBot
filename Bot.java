622a4eb2d47f9e003f3625963f35f4745cb06c0c
Nicholas Smirnov <79801331+Nicholas-Smirnov@users.noreply.github.com>
2022-09-03 22:56:29 -0400

    Create Bot.java

---
+++ Bot.java
@@ -0,0 +1,17 @@
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.*;
+
+public class Television {
+	public static JDA ourJDA;
+	public static String prefix = "~";
+
+	public static void main(String[] args) throws LoginException {
+
+		ourJDA = new JDABuilder(AccountType.BOT).setToken("THETOKEN").build();
+		ourJDA.getPresence().setStatus(OnlineStatus.IDLE);
+		ourJDA.getPresence().setGame(Game.watching(""));
+		// These commands we get from the Commands.java
+		ourJDA.addEventListener(new Commands());
+	}
+}
